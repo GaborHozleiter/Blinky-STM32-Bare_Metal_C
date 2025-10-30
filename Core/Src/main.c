bd1e333f2d8275c4ac80b9c9a863ad8e7d59548a
Gabor Hozleiter <hozleitergabor@gmail.com>
2025-10-30 20:21:19 +0100

    Update documentation and code comments

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -2,16 +2,22 @@
 
 int main(void){
 
+	// ---------------- Clock Configuration -------------------------------------
+	// Enable GPIOA as GPIOC clocks
 	RCC->IOPENR |= (1U << 0) | (1U << 2);
 
+	// ---------------- GPIO Configuration --------------------------------------
+	// Configure PA5 as output (LED pin)
 	GPIOA->MODER &= ~((1U << 11) | (1U << 10));
-
-	GPIOC->MODER &= ~((1U << 27) | (1U << 26));
 
 	GPIOA->MODER |= (1U << 10);
 
-	GPIOA->BSRR = (1U << 5);
 
-	// Small change
+	// Configure PA13 as input (user button)
+	GPIOC->MODER &= ~((1U << 27) | (1U << 26));
+
+	// ------------------ LED Control -------------------------------------------
+	// Turn LED on (PA5 high)
+	GPIOA->BSRR = (1U << 5);
 
 }
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-Basic LED blinking project
+A basic STM32 projectrunning on the Nucleo-G070RB board.
+
+Currently, it toggles an LED connected to PA5 and uses a button on PC13 for input.
+
+Planned Features:
+
+- Replace button polling with EXTI interrupt handling
+
+- Add a watchdog timer that triggers if the button is not pressen within 10 seconds
+
+
+This project is a starting point and will be gradually extended as part of my STM32 learning path. 
+
