242f5607a5cbd84db2cfdea17a717c11bf3c8b40
Gabor Hozleiter <hozleitergabor@gmail.com>
2025-10-30 18:35:02 +0100

    Initial commit

---
+++ Core/Src/main.c
@@ -0,0 +1,15 @@
+#include "stm32g0xx.h";
+
+int main(void){
+
+	RCC->IOPENR |= (1U << 0) | (1U << 2);
+
+	GPIOA->MODER &= ~((1U << 11) | (1U << 10));
+
+	GPIOC->MODER &= ~((1U << 27) | (1U << 26));
+
+	GPIOA->MODER |= (1U << 10);
+
+	GPIOA->BSRR = (1U << 5);
+
+}
