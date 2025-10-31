58301451c4a251281f0d44c944c1c1e047a5f882
Gabor Hozleiter <hozleitergabor@gmail.com>
2025-10-31 07:36:24 +0100

    Add main loop and button-press logic

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,4 +1,4 @@
-#include "stm32g0xx.h";
+#include "stm32g0xx.h"
 
 int main(void){
 
@@ -16,8 +16,49 @@ int main(void){
 	// Configure PA13 as input (user button)
 	GPIOC->MODER &= ~((1U << 27) | (1U << 26));
 
-	// ------------------ LED Control -------------------------------------------
-	// Turn LED on (PA5 high)
-	GPIOA->BSRR = (1U << 5);
+
+	// Activate internal pull-up for PC13
+	GPIOC->PUPDR &= ~((1U << 26) | (1U << 27));
+
+	GPIOC->PUPDR |= (1U << 26);
+
+
+
+	// Start the main application loop
+	while(1){
+
+	// Check for the button press
+	if (!(GPIOC->IDR & (1U << 13))) {
+
+		// Simple delay for debouncing the button press
+		for(int i=0; i<50000; i++){}
+
+		// Check again after the delay
+		if (!(GPIOC->IDR & (1U << 13))) {
+					// ------------------ LED Control -------------------------------------------
+						// Turn LED on (PA5 high)
+						GPIOA->BSRR = (1U << 5);
+				}
+			else {
+
+				// ------------------ LED Control -------------------------------------------
+				// Turn LED off (PA5 low)
+			GPIOA->BSRR = (1U << 21);
+			}
+		}
+	else {
+
+		// Simple delay for debouncing the button press
+		for(int i=0; i<50000; i++){}
+
+		// Check again after the delay
+		if ((GPIOC->IDR & (1U << 13))) {
+
+			// ------------------ LED Control -------------------------------------------
+			// Turn LED off (PA5 low)
+			GPIOA->BSRR = (1U << 21);
+			}
+	}
+	}
 
 }
